bb9441a8d5483de2169e80167cea41a0b8a0f856
vrmsreddy <vrms@ms.com>
2017-10-16 13:55:42 +0000

    Added CRUD in same Transaction

--- spring-data/sql/ms-jpa-audit/src/main/java/com/ms/jpa/audit/controller/CityController.java
+++ spring-data/sql/ms-jpa-audit/src/main/java/com/ms/jpa/audit/controller/CityController.java
@@ -5,11 +5,14 @@ import com.ms.jpa.audit.repository.CityRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/cities")
 public class CityController {
-
+    private Logger LOGGER = LoggerFactory.getLogger(City.class);
+    
     @Autowired
     private CityRepository cityRepository;
 
@@ -41,6 +44,28 @@ public class CityController {
     public boolean delete(@PathVariable Integer cityCode) {
         cityRepository.delete(cityCode);
         return true;
+    }
+    
+    @RequestMapping(value = "/crud", method = RequestMethod.POST)
+    public City crudAllTest(@RequestBody City city) {
+    	//create
+    	cityRepository.save(city);
+    	//retrive
+    	City cityNew = cityRepository.findOne(city.getCityCode());
+    	LOGGER.info("SAVE&RETRIVE"+cityNew);
+    	
+    	//update
+    	cityNew.setName(cityNew.getName()+"Dummy");
+    	cityRepository.save(cityNew);
+    	cityNew = cityRepository.findOne(city.getCityCode());
+    	LOGGER.info("UPDATED"+cityNew);
+    	
+    	//delete
+        cityRepository.delete(city.getCityCode());
+        cityNew = cityRepository.findOne(city.getCityCode());
+        LOGGER.info("DELETED"+cityNew);
+        
+        return cityNew;
     }
 
 
